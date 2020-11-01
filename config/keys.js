f4516a6331380dd09b906e7133e9493c49d95c41
loveranes <59357602+hloveranes@users.noreply.github.com>
2020-11-01 12:49:31 +0800

    removed credentials

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mdb_user:EKV91VJi9awchZvN@cluster-techy.gxakw.gcp.mongodb.net/Cluster-Techy?retryWrites=true&w=majority",
-    secret: 'yoursecret'
 
     // mongoURI: "mongodb+srv://<user>:<password>@<cluster>.gxakw.gcp.mongodb.net/<db_name>?retryWrites=true&w=majority",
     // secret: 'yoursecret'
